48e62bb5d0e0194b4a4cc1dc84728bdce5607fbe
Taesub Yun <taesubyun@icloud.com>
2022-03-30 16:11:46 +0900

    video파일 추가 및 임시 사이즈 입력

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 function Home() {
-    return <></>;
+    return (
+        <>
+            <video
+                autoPlay
+                muted
+                loop
+                playsinline
+                width='100%'
+                height='445px'
+                style={{
+                    objectFit: 'fill',
+                    objectPosition: 'center',
+                    width: '100%',
+                    height: '470px',
+                }}
+            >
+                <source src='/assets/video.mp4' type='video/mp4' />
+            </video>
+        </>
+    );
 }
 
 export default Home;
