b658eed8661693d4c4aa564266b91a9b8e4b79d0
Taesub Yun <taesubyun@icloud.com>
2022-03-30 14:45:48 +0900

    Header 최초  레이아웃 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
-    return <div className='App'>초기 설정</div>;
+    return (
+        <div className='App'>
+            <Header />
+            <Home />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+    return (
+        <Container>
+            <HeaderLogo src='/assets/prap.png' />
+            <HeaderMenu>
+                <a style={{ alignItems: 'center', paddingLeft: '120px' }}>
+                    <div style={{ fontSize: '20px' }}>홈</div>
+                </a>
+                <a style={{ alignItems: 'center', paddingLeft: '54px' }}>
+                    <div style={{ fontSize: '20px', letterSpacing: '5px' }}>
+                        탐색하기
+                    </div>
+                </a>
+            </HeaderMenu>
+            <a
+                style={{
+                    alignItems: 'center',
+                    display: 'flex',
+                    fontSize: '20px',
+                    float: 'right',
+                    paddingRight: '50px',
+                }}
+            >
+                <img
+                    src='/assets/search.png'
+                    style={{
+                        width: '20px',
+                        padding: '0 35px 0 15px',
+
+                        cursor: 'pointer',
+                    }}
+                />
+                <div style={{ width: '100px', fontSize: '20px' }}>로그인</div>
+            </a>
+        </Container>
+    );
+}
+
+export default Header;
+
+const Container = styled.nav`
+    height: 70px;
+    display: flex;
+    align-items: center;
+    padding-left: 55px;
+    box-shadow: 1px 1px 8px 0px;
+`;
+
+const HeaderLogo = styled.img`
+    width: 90px;
+`;
+
+const HeaderMenu = styled.div`
+    display: flex;
+    width: 100%;
+`;
