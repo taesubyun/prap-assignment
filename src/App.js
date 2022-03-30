280515f4df546a1323b53b84b051d01cb22f721d
Taesub Yun <taesubyun@icloud.com>
2022-03-30 16:21:14 +0900

    commit 오류 최종 수정 완료

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import './App.css';
 import Header from './components/Header';
-<<<<<<< HEAD
+
 import Home from './components/Home';
 import ContentSlider from './components/ContentSlider';
-=======
->>>>>>> 13722790523f5cf113e5eacdd0d07ec8ce7186a5
 
 function App() {
     return (
         <div className='App'>
             <Header />
-<<<<<<< HEAD
+
             <Home />
             <ContentSlider />
-=======
->>>>>>> 13722790523f5cf113e5eacdd0d07ec8ce7186a5
         </div>
     );
 }
