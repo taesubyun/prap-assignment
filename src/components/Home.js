8642621132a10aeec1d9d6bd776fd017e38e0427
Taesub Yun <taesubyun@icloud.com>
2022-03-31 13:29:35 +0900

    폰트 변경

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,7 @@ import styled from 'styled-components';
 function Home() {
     return (
         <div className='container' style={{ position: 'relative' }}>
-            <div
-                className='vidContainer'
-                style={{ position: 'relative', zIndex: '0' }}
-            >
+            <div className='vidContainer' style={{ position: 'relative' }}>
                 <video
                     className='faceVideo'
                     autoPlay
@@ -31,19 +28,19 @@ function Home() {
                         top: '-150px',
                         left: '120px',
                         width: 'calc(100% - 240px)',
-                        height: '340px',
-                        borderRadius: '22px',
-                        backgroundColor: 'white',
-                        padding: '15px 15px',
+                        height: '100%',
+                        borderRadius: '8px',
+                        backgroundColor: 'rgb(254, 254, 254)',
+                        boxShadow: 'rgb(0 0 0 / 10%) 0px 0px 15px 0px',
+                        padding: '20px 20px',
                     }}
                 >
                     <div
                         style={{
-                            border: '2px solid white',
-                            padding: '10px 20px',
+                            padding: '10px 15px',
                         }}
                     >
-                        <div style={{ border: '2px solid black' }}>
+                        <div>
                             <h2
                                 style={{
                                     textAlign: 'center',
@@ -76,26 +73,52 @@ function Home() {
                         <ProductWrapper>
                             <li>
                                 <div>
-                                    <ProductImg src='./assets/product1.png'></ProductImg>
+                                    <ProductImg src='./assets/nft.png'></ProductImg>
                                 </div>
                                 <div>
-                                    <h5
-                                        style={{
-                                            fontSize: '18px',
-                                            fontWeight: '400',
-                                            lineHeight: '27px',
-                                            margin: '16px 0px 0px',
-                                            textAlign: 'center',
-                                            width: '100%',
-                                        }}
-                                    >
-                                        NFT
-                                    </h5>
+                                    <ContainerTitle>NFT</ContainerTitle>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <ProductImg src='./assets/sneakers.png'></ProductImg>
+                                </div>
+                                <div>
+                                    <ContainerTitle>
+                                        스니커즈 리셀
+                                    </ContainerTitle>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <ProductImg src='./assets/piece.png'></ProductImg>
+                                </div>
+                                <div>
+                                    <ContainerTitle>P2P 투자</ContainerTitle>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <ProductImg src='./assets/funding.png'></ProductImg>
+                                </div>
+                                <div>
+                                    <ContainerTitle>
+                                        스타트업 펀딩
+                                    </ContainerTitle>
+                                </div>
+                            </li>
+                            <li>
+                                <div>
+                                    <ProductImg src='./assets/stock.png'></ProductImg>
+                                </div>
+                                <div>
+                                    <ContainerTitle>비상장 주식</ContainerTitle>
                                 </div>
                             </li>
                         </ProductWrapper>
                     </div>
                 </div>
+                <Button>모든 투자상품 확인하기</Button>
             </div>
         </div>
     );
@@ -104,6 +127,12 @@ export default Home;
 
 const ProductWrapper = styled.ul`
     display: flex;
+
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 10px;
+    list-style: none;
+    box-sizing: border-box;
 `;
 
 const ProductImg = styled.img`
@@ -112,3 +141,35 @@ const ProductImg = styled.img`
     fill: none;
 `;
 // fill : none??
+// Container 정하기
+const ContainerTitle = styled.h5`
+    font-size: 18px;
+    font-weight: 400;
+    margin: 16px 0 0;
+    text-align: center;
+    width: 100%;
+`;
+
+const Button = styled.button`
+    position: absolute;
+    bottom: 88px;
+    left: calc(50% - 108px);
+    width: 235px;
+    height: 45px;
+    border-radius: 52px;
+    border: none;
+    cursor: pointer;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+    background-color: rgb(107, 229, 182);
+}`;
+
+//   border: '2px solid blue',
+//                         borderRadius: '8px',
+//                         backgroundColor: 'rgb(254, 254, 254)',
+//                         boxShadow: 'rgb(0 0 0 / 10%) 0px 0px 15px 0px',
+//                         padding: ' 32px 60px 40px',
+//                         // position: 'absolute',
+//                         // width: 'calc (100% - 240px)',
+//                         maxWidth: '1200px',
